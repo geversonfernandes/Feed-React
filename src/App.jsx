34e2ec069e1b9a29958b3913e43679f16b810d2b
geversonfernandes <gever.fernandes@gmail.com>
2023-04-17 18:27:26 -0300

    add: sidebar content

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import { Header } from './components/Header'
 import { Post } from "./Post"
 
+import styles from './App.module.css'
 import './global.css'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author='Geverson Araujo'
-        content=' Lorem ipsum dolor sit amet consectetur adipisicing elit. Est dolore ipsam doloremque ex voluptates sunt illo ullam, corrupti mollitia quo, accusantium vel corporis nihil porro doloribus exercitationem ut sint earum!'
-      />
-      <Post
-        author='Gabriel Buzzi'
-        content='Um post muito legal'
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author='Geverson Araujo'
+            content=' Lorem ipsum dolor sit amet consectetur adipisicing elit. Est dolore ipsam doloremque ex voluptates sunt illo ullam, corrupti mollitia quo, accusantium vel corporis nihil porro doloribus exercitationem ut sint earum!'
+          />
+          <Post
+            author='Gabriel Buzzi'
+            content='Um post muito legal'
+          />
+        </main>
+      </div>
     </div>
   )
 }

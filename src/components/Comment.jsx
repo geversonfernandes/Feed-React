74868acc20da85265d316b113d9ff05b2b2f6a48
geversonfernandes <gever.fernandes@gmail.com>
2023-04-22 18:46:12 -0300

    add: comments and styles component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,30 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/geversonfernandes.png" alt="" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Geverson Araujo</strong>
+              <time title='18 de abril ás 02:28' dateTime='2022-04-18 02:27:00'>Cerca de 1h atrás</time>
+            </div>
+            <button title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}

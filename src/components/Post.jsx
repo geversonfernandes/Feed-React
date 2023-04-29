03650d6e80a82b5233e45a5336a840f29ab553c3
geversonfernandes <gever.fernandes@gmail.com>
2023-04-29 08:37:58 -0300

    add: avatar and styles component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
@@ -6,7 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img src="https://github.com/geversonfernandes.png" />
+          <Avatar src="https://github.com/geversonfernandes.png" />
           <div className={styles.authorInfo}>
             <strong>Geverson Araujo</strong>
             <span>Web Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
-
 import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -9,9 +9,7 @@ export function Sidebar() {
         className={styles.cover}
         src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/geversonfernandes.png" />
+        <Avatar src="https://github.com/Scytar.png" />
         <strong>Geverson Araujo</strong>
         <span>Web Developer</span>
       </div>
